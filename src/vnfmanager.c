3a7fec1439b682c8631a6d37e0d6a9ff9de8ab84
David Cho <dwjjang2@gmail.com>
2019-01-22 00:07:25 +0900

    2019-01-21

---
+++ src/vnfmanager.c
@@ -0,0 +1,20 @@
+/**
+ VNF Manager: Algorithms, VNF reallocation strategy
+ 
+ For Performance
+ 1. VNF slicing
+ 2. VNF scale-out
+ 
+ 
+**/
+
+int main(int argc, char *argv[])
+{
+    VNF *vnf = (VNF *)malloc(sizeof(VNF));
+
+    
+    while(1)
+    {
+    }
+    return 1;
+}
