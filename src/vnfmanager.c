ee07824ad859648505ed6ef85355eaafe47c0550
Daewoong-Cho <dwjjang2@gmail.com>
2019-01-22 16:21:30 +0900

    Update vnfmanager.c

--- src/vnfmanager.c
+++ src/vnfmanager.c
@@ -8,13 +8,21 @@
  
 **/
 
+#define THRESHOLD 0.5
+
 int main(int argc, char *argv[])
 {
-    VNF *vnf = (VNF *)malloc(sizeof(VNF));
-
+  VNF *vnf = (VNF *)malloc(sizeof(VNF));
+  vnf = vnf_list->head;
     
-    while(1)
-    {
-    }
-    return 1;
+  while(1)
+  {
+   if(vnf->vnf_usage < THRESHOLD && vnf->in_queue->queue_usage > THRESHOLD)
+   {
+     VNF *new_vnf = (VNF *)malloc(sizeof(VM *));
+     n
+     //change setting
+   }
+  }
+  return 1;
 }
